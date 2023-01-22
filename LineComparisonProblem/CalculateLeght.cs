ba9eb53428ed388b848a1be403f7f88907914a97
Snehal979 <bansodsnehal535@gmail.com>
2023-01-23 01:42:27 +0530

    [Snehal]Add: Comparing Line are EqualOr Not Equal

--- LineComparisonProblem/CalculateLeght.cs
+++ LineComparisonProblem/CalculateLeght.cs
@@ -8,7 +8,9 @@ namespace LineComparisonProblem
 {
     internal class CalculateLeght
     {
+        //Variable 
         int x1, y1, x2, y2;
+        //Constructor
         public CalculateLeght(int x1, int y1, int x2, int y2)
         {
             this.x1=x1;
@@ -16,10 +18,11 @@ namespace LineComparisonProblem
             this.x2=x2;
             this.y2=y2;
         }
-        public void LenghtOfLine()
+        
+        public double LenghtOfLine()
         {
             double lengthOfLine = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2));
-            Console.WriteLine("Length of the line is = " + lengthOfLine);
+            return lengthOfLine;
         }
     }
 }
--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -5,9 +5,25 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome Line Comparison Program");
-            CalculateLeght calculateLeght = new CalculateLeght(1, 4, 2, 8);
-            calculateLeght.LenghtOfLine();
-            Console.ReadLine();
+            //For Line 1 Object
+            CalculateLeght calculateLeght_1 = new CalculateLeght(1, 4, 2, 8);
+            double Line_1 = calculateLeght_1.LenghtOfLine();
+            Console.WriteLine("Lenght of First line = "+Line_1);
+            //For Line 2 Object
+            CalculateLeght calculateLeght_2 = new CalculateLeght(5, 4, 2, 8);
+            double Line_2 = calculateLeght_2.LenghtOfLine();
+            Console.WriteLine("Lenght of Second line = "+Line_2);
+
+            //Uc 2  Equal method(given boolen condition)
+            if(Line_1.Equals(Line_2))
+            {
+                Console.WriteLine("Both Line Are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not Both Line Are Equal");
+            }
+
         }
     }
 }
