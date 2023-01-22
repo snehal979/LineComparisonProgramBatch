1c43408b13ad2cedaa3923d33225a5a6e4e8f02d
Snehal979 <bansodsnehal535@gmail.com>
2023-01-23 01:30:45 +0530

    [Snehal]Add: cCalculate the lenghth of the Line

---
+++ LineComparisonProblem/CalculateLeght.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    internal class CalculateLeght
+    {
+        int x1, y1, x2, y2;
+        public CalculateLeght(int x1, int y1, int x2, int y2)
+        {
+            this.x1=x1;
+            this.y1=y1;
+            this.x2=x2;
+            this.y2=y2;
+        }
+        public void LenghtOfLine()
+        {
+            double lengthOfLine = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2));
+            Console.WriteLine("Length of the line is = " + lengthOfLine);
+        }
+    }
+}
--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -5,6 +5,9 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome Line Comparison Program");
+            CalculateLeght calculateLeght = new CalculateLeght(1, 4, 2, 8);
+            calculateLeght.LenghtOfLine();
+            Console.ReadLine();
         }
     }
 }
