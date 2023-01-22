a4014fa00f7bbeaa564df361b018a61dd696807e
Snehal979 <bansodsnehal535@gmail.com>
2023-01-23 01:58:54 +0530

    [Snehal]Add: Comparison of Two Line By method

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -10,7 +10,7 @@
             double Line_1 = calculateLeght_1.LenghtOfLine();
             Console.WriteLine("Lenght of First line = "+Line_1);
             //For Line 2 Object
-            CalculateLeght calculateLeght_2 = new CalculateLeght(5, 4, 2, 8);
+            CalculateLeght calculateLeght_2 = new CalculateLeght(5, 9, 9, 8);
             double Line_2 = calculateLeght_2.LenghtOfLine();
             Console.WriteLine("Lenght of Second line = "+Line_2);
 
@@ -22,8 +22,18 @@
             else
             {
                 Console.WriteLine("Not Both Line Are Equal");
+                // To check which Line is greater By CompareTo Method
+                if (Line_1.CompareTo(Line_2) > 0)
+                {
+                    Console.WriteLine("Line 1 is Greater to Line 2");
+                }
+                else
+                {
+                    Console.WriteLine("Line 1 is Less than Line 2");
+                }
             }
 
+            Console.ReadLine();
         }
-    }
+    }  
 }
