6c3f730f4c8a2b9773508e1c638f065f92ba0318
Snehal979 <bansodsnehal535@gmail.com>
2023-01-31 21:07:18 +0530

    [Snehal]Add: Line Comparison By equal or CompareMethod

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -10,28 +10,25 @@
             double Line_1 = calculateLeght_1.LenghtOfLine();
             Console.WriteLine("Lenght of First line = "+Line_1);
             //For Line 2 Object
-            CalculateLeght calculateLeght_2 = new CalculateLeght(5, 9, 9, 8);
+            CalculateLeght calculateLeght_2 = new CalculateLeght(5, 9, 6, 8);
             double Line_2 = calculateLeght_2.LenghtOfLine();
             Console.WriteLine("Lenght of Second line = "+Line_2);
 
-            //Uc 2  Equal method(given boolen condition)
-            if(Line_1.Equals(Line_2))
+            // To check which Line is greater By CompareTo Method
+            if (Line_1.CompareTo(Line_2) > 0)
             {
-                Console.WriteLine("Both Line Are Equal");
+                Console.WriteLine("Line 1 is Greater to Line 2");
+            }
+            else if(Line_2.CompareTo(Line_1) > 0)
+            {
+                Console.WriteLine("Line 1 is Less than Line 2");
             }
             else
             {
-                Console.WriteLine("Not Both Line Are Equal");
-                // To check which Line is greater By CompareTo Method
-                if (Line_1.CompareTo(Line_2) > 0)
-                {
-                    Console.WriteLine("Line 1 is Greater to Line 2");
-                }
-                else
-                {
-                    Console.WriteLine("Line 1 is Less than Line 2");
-                }
+                Console.WriteLine("Both Line Are Equal");
             }
+
+
 
             Console.ReadLine();
         }
